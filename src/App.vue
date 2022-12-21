d15885d5a212b05742b247163cb403ddbd4f6aa4
Adit Tanu <adiettanu27@gmail.com>
2022-12-21 10:18:48 +0700

    click event modifier

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
   <div v-if="showModal">
     <Modal :header="header" :text="text" theme="light" @close="toggleModal"/>
   </div>
-  <button @click="toggleModal">Open Modal</button>
+  <button @click.alt="toggleModal">Open Modal (alt)</button>
 </template>
 
 <script>
